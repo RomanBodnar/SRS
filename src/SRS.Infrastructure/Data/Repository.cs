781540ad3f4cc012eda70ed774fa7b345bf492ad
Roman Bodnar <wizzardrince@gmail.com>
2019-05-10 17:10:07 +0300

    SRS Added repostory and UoW classes

---
+++ src/SRS.Infrastructure/Data/Repository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SRS.Core.Interfaces;
+
+namespace SRS.Infrastructure.Data
+{
+    public class Repository : IRepository
+    {
+    }
+}
